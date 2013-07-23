f860b0956303a84482c8665d66182b9fceb58746
Usman <usmanfareed92@gamil.com>
2013-07-23 15:43:40 +0500

    commit

---
+++ scripts/js_library.js
@@ -0,0 +1,12 @@
+
+if($('#showcase').length) {
+	$.include('scripts/libs/jquery.aw-showcase.min.js')
+}
+
+if($('#jplayer').length) {
+    $.include('scripts/libs/jquery.jplayer.min.js');
+
+if($('.star').length) {$.include('scripts/libs/jquery.raty.min.js');}
+if($('#scroll-pane').length) {$.include('scripts/libs/jquery.mousewheel.min.js');}
+if($('#scroll-pane').length) {$.include('scripts/libs/jquery.jscrollpane.min.html');}
+}
