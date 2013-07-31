983695d8ef33c0f6d171bfe5f9c586d2b1e5483f
Usman <usmanfareed92@gamil.com>
2013-07-31 14:31:04 +0500

    some changes

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,3 +1,4 @@
+<footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
@@ -37,5 +38,4 @@
 
 <script type="text/javascript" src="assets/js/style-switcher.js"></script>
 
-</body>
-</html>
+</footer>
