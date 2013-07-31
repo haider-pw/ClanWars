e38b8c132e3c5c4ca1311f20bbcd174166e88d40
Usman <usmanfareed92@gamil.com>
2013-07-31 10:53:47 +0500

    some bugs in profile view

--- application/views/user_pages/profile_edit.php
+++ application/views/user_pages/profile_edit.php
@@ -1,9 +1,9 @@
-<div class="span9" ng-controller="controller">
+<div class="span9">
 
     <!-- About -->
     <div class="heading">
         <h4>Mr.Awesome</h4>
-        <a id="button" href="#" ng-click="update()" >Done</a><br>
+        <a id="button" href="#" onclick="update()" >Done</a><br>
     </div>
     <div class="menubar links primary">
         <ul class="site">
@@ -14,32 +14,48 @@
     <h3>About</h3>
     <textarea class="textarea"></textarea>
     <!-- // About END -->
+    <?php echo form_open_multipart('users/register_user',array('id' => 'register_user','name' => 'register_user')); ?>
     <form class="form" ng-repeat="user in userdata">
-
-        <label for="Name">User Name:</label><input type="text" id="Name" ng-model="user.UserName"><br>
-        <label for="GameName">Game Name:</label><input type="text" id="GameName" ng-model="user.InGameName"><br>
-        <label for="Cnic">Cnic:</label><input type="text" id="cnic" ng-model="user.Cnic"><br>
-        <label for="Email">Email:</label><input type="text" id="Email" ng-model="user.Email"><br>
-        <label for="Password">User Name:</label><input type="text" id="Password" ng-model="user."><br>
-        <label for="Age">Age:</label><input type="text" id="Age" ng-model="user.Age"><br>
+    <label for="Name">User Name:</label><input type="text" id="Name" ><br>
+        <label for="GameName">Game Name:</label><input type="text" id="GameName" ><br>
+        <label for="Cnic">Cnic:</label><input type="text" id="cnic" ><br>
+        <label for="Email">Email:</label><input type="text" id="Email" ><br>
+        <label for="Password">User Name:</label><input type="text" id="Password" ><br>
+        <label for="Age">Age:</label><input type="text" id="Age" ><br>
         <label for="Gender">Gender:</label>
-        <label class="gender" for="male">Male:</label><input style="margin-left: -100px;"   type="radio" id="male" name="gen" ng-model="user.Gander" >
-        <label class="gender" for="female">Female:</label><input style="margin-left: -85px;" type="radio" id="female" name="gen" ng-model="user.Gender" value="Female">
-
-    </form>
+        <label class="gender" for="male">Male:</label><input style="margin-left: -100px;"   type="radio" id="male" name="gen"  >
+        <label class="gender" for="female">Female:</label><input style="margin-left: -85px;" type="radio" id="female" name="gen" value="Female">
+</form>
 </div>
 
-<script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $("#submitBtn").click(function() {
+            submit_form();
+        });
+    });
 
-    function controller ($scope,$http)
-    {
-        var formupdate="<?php echo base_url('index.php/users/update_profile'); ?>"
-        $scope.update=function()
-        {
-            $http.post(formupdate, $scope.user);
+    function  submit_form(){
+        $("#register_user").ajaxSubmit({
+            success: function(response){
+                var data = response.split(":::")
+                if(data[0] == 'error')
+                {
+                    fading_msg("error_message",data[1]);
+                }else{
+                    window.location.href = "<?php base_url('index.php/main'); ?>";
+                    fading_msg("notification_message","Profile Updated Successfully");
+                }
+            },
+            error:function(xhr,error_status,error_thrown){
+                //fading_msg("error_message",error_thrown);
+                alert(error_thrown);
+            }
 
-            alert($scope.user);
-        }
-
+        });
     }
+
+
+
+
 </script>
