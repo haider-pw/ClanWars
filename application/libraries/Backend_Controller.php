5ec8fbdde5d6b72a52482bf34a1c29bb4cde2074
pakistanihaider <pakistan>
2013-07-31 12:47:00 +0500

    Added Base Controllers for Frontend and Backend..

---
+++ application/libraries/Backend_Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pakistanihaider
+ * Date: 08/06/13
+ * Time: 22:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Backend_Controller extends MY_Controller
+{
+    public function __construct(){
+        parent::__construct();
+    }
+}
---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: SBPS
+ * Date: 7/31/13
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Frontend_Controller extends MY_Controller
+{
+    public function __construct(){
+        parent::__construct();
+    }
+}
