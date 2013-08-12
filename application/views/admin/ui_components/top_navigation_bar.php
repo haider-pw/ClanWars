3bebdf4b567b2c2542ab5db615f6fa1919bfae05
Usman <usmanfareed92@gamil.com>
2013-08-12 14:43:46 +0500

    settings in tabs

--- application/views/admin/ui_components/top_navigation_bar.php
+++ application/views/admin/ui_components/top_navigation_bar.php
@@ -41,26 +41,9 @@
                        href="login.html"><i class="icon-off"></i></a></div>
             </div>
             <!-- /.topnav -->
-            <div class="nav-collapse collapse">
-                <!-- .nav -->
-                <ul class="nav">
-                    <li class="active"><a href="index.html">Dashboard</a></li>
-                    <li><a href="table.html">Tables</a></li>
-                    <li><a href="file.html">File Manager</a></li>
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            Form Elements <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="form-general.html">General</a></li>
-                            <li><a href="form-validation.html">Validation</a></li>
-                            <li><a href="form-wysiwyg.html">WYSIWYG</a></li>
-                            <li><a href="form-wizard.html">Wizard &amp; File Upload</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- /.nav -->
-            </div>
+<?php
+$this->load->view('admin/ui_components/top_tabs');
+?>
         </div>
     </div>
 </div>
---
+++ application/views/admin/ui_components/top_tabs.php
@@ -0,0 +1,18 @@
+<div class="nav-collapse collapse">
+<ul class="nav">
+    <li class="active"><a href="<?php echo base_url('admin/dashboard');?>">Dashboard</a></li>
+    <li><a href="<?php echo base_url('admin/dashboard');?>">Users</a></li>
+    <li><a href="file.html">File Manager</a></li>
+    <li class="dropdown">
+        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+            Form Elements <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+            <li><a href="form-general.html">General</a></li>
+            <li><a href="form-validation.html">Validation</a></li>
+            <li><a href="form-wysiwyg.html">WYSIWYG</a></li>
+            <li><a href="form-wizard.html">Wizard &amp; File Upload</a></li>
+        </ul>
+    </li>
+</ul>
+</div>
